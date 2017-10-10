724505724865053c251c28b66bfbb747c9b755d0
Dwij Sukeshkumar Sheth <dwijhariket@gmail.com>
2017-10-10 11:58:04 -0700

    Add files via upload

--- test2.py
+++ test2.py
@@ -10,24 +10,23 @@ ser = serial.Serial('COM11', 250000, timeout=0)
 #in seconds
 
 
-fixed_interval = 1
+fixed_interval = 60
 while 1:
   x = []
   try:
     #temperature value obtained from Arduino + DH11 Temp Sensor          
-    temperature_c = ser.readline().decode('utf-8')
-    x=re.split(" ",temperature_c)
+    sensor_c = ser.readline().decode('utf-8')
+    x=re.split(" ",sensor_c)
     #current time and date
     time_hhmmss = time.strftime('%H:%M:%S')
     date_mmddyyyy = time.strftime('%d/%m/%Y')
     #current location name
-    temperature_location = 'VIT-CHENNAI';
     #print (temperature_c + ',' + time_hhmmss + ',' + date_mmddyyyy + ',' + temperature_location)
      
   #insert record
     if(len(x)==5):
       data = {'date':date_mmddyyyy,'time':time_hhmmss,'Temperature':x[0],'Humidity':x[1],'HeartRate_constant':x[2],'Shock Switch':x[3],'Alert Button Press Status':x[4]}
-      result = requests.post(firebase_url + '/' + temperature_location + '/temperature.json', data=json.dumps(data))
+      result = requests.post(firebase_url + '/sensor.json', data=json.dumps(data))
       print ('Record inserted. Result Code = ' + str(result.status_code) + ',' + result.text)
       time.sleep(fixed_interval)
     else:
