11f4e6025a62621239db80147155b6600e8233c9
Dwij Sukeshkumar Sheth <dwijhariket@gmail.com>
2017-10-10 10:22:16 -0700

    Add files via upload

--- sketch_oct10a/sketch_oct10a.ino
+++ sketch_oct10a/sketch_oct10a.ino
@@ -17,27 +17,34 @@ void loop ()
 {
     static double oldValue = 0;
     static double oldChange = 0;
+    byte temperature = 0;
+    
  
     int rawValue = analogRead (sensorPin);
+    int tilt=0;
     double value = alpha * oldValue + (1 - alpha) * rawValue;
  
-    byte temperature = 0;
+    
   byte humidity = 0;
   int err = SimpleDHTErrSuccess;
-  if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
-    return;
-  }
-  Serial.println((int)temperature);Serial.print("\t"); Serial.print((int)humidity);Serial.print("\t");Serial.print(value);
-    oldValue = value;
-    
   if (digitalRead(5) == LOW){
     digitalWrite(13, HIGH);
-    Serial.print("\t");Serial.print(1);// turn ON LED
+    tilt=1;
   }
   else{
     digitalWrite(13, LOW);
-    Serial.print("\t");Serial.print(0);
+    tilt=0;
   }
+  if ((err = dht11.read(pinDHT11, &temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    return;
+  }
+  //Serial.println(temperature,humidity,value,tilt);
+    oldValue = value; 
+  String thisString_1=String(temperature);
+  String thisString_2=String(humidity);
+  String thisString_3=String(value);
+  String thisString_4=String(tilt);
+  Serial.println(thisString_1+" "+thisString_2+" "+thisString_3+" "+thisString_4);
  
     delay (period);
 }
